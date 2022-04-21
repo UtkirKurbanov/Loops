a19b1a3c83ebd609e19d99a43ccee5eec8e81033
Utkir <karshi2700@mail.ru>
2022-04-21 08:59:12 +0500

    Working with Loops in PHP

---
+++ loops.php
@@ -0,0 +1,21 @@
+<?php
+
+    // $num = 1;
+
+    // while ($num <= 10) {
+    //     echo $num."<br>";
+    //     $num++;
+    // }
+
+    // $son = 1;
+    // do {
+    //     echo $son."<br>";
+    //     $son++;
+    // } while ($son <= 10);
+
+        
+    for ($i = 1; $i <= 10; $i++) { 
+        echo $i."<br>";
+    }    
+    
+?>
